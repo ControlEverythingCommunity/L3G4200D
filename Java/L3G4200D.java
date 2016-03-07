ce07be2f5317d4566f8d7d5dfc5dd1a8a5167780
Yadwinder Singh <ryker1990@gmail.com>
2016-03-07 15:30:09 +0530

    Change in java filename

--- Java/L3G4200D.java
+++ Java/L3G4200D.java
@@ -9,7 +9,7 @@ import com.pi4j.io.i2c.I2CDevice;
 import com.pi4j.io.i2c.I2CFactory;
 import java.io.IOException;
 
-public class L3G4200
+public class L3G4200D
 {
 	public static void main(String args[]) throws Exception
 	{
